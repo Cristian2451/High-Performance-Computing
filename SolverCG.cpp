438f95fcfd6130bbee311892af3a612115f80112
Cristian Vegas <74818424+Cristian2451@users.noreply.github.com>
2024-04-20 17:06:01 +0100

    Update SolverCG.cpp

--- SolverCG.cpp
+++ SolverCG.cpp
@@ -23,8 +23,8 @@ SolverCG::SolverCG(int pNx, int pNy, double pdx, double pdy)
     dy = pdy;
     Nx = pNx;
     Ny = pNy;
-	v = new double[Nx*Ny]();
-	s = new double[Nx*Ny]();
+    v = new double[Nx*Ny]();
+    s = new double[Nx*Ny]();
 }
 
 // Destructor for SolverCG class
@@ -34,26 +34,25 @@ SolverCG::~SolverCG()
     delete[] b;
     delete[] x;
     delete[] r;
-	delete[] x_full;
+    delete[] x_full;
     delete[] p;
-	delete[] p_layered;
-	delete[] x_sum;
+    delete[] p_layered;
+    delete[] x_sum;
     delete[] z;
     delete[] t;
-	delete[] v;
+    delete[] v;
     delete[] s;
 }
 
 /**
     @brief Function to perform domain partition and send the respective sub-domain information to each MPI process
     @param Nx Number of grid-points in x-direction
-	@param Ny Number of grid-points in y-direction
-	@param p Number of partition in the x and y directions
+    @param Ny Number of grid-points in y-direction
+    @param p Number of partition in the x and y directions
     @return void
 */
-void SolverCG::SetMPI(int Nx, int Ny, int p)
-{
-	part = p;
+void SolverCG::SetMPI(int Nx, int Ny, int p){
+    part = p;
     // Check if MPI initalised
     MPI_Initialized(&MPIInit);
     if (!MPIInit){
@@ -108,9 +107,9 @@ void SolverCG::SetMPI(int Nx, int Ny, int p)
                 int* process_end_x = new int [size];
                 int* process_start_y = new int [size];
                 int* process_end_y = new int [size];
-				int* length = new int [size];
-				int* count_x = new int [size];
-				int* count_y = new int [size];
+		int* length = new int [size];
+		int* count_x = new int [size];
+		int* count_y = new int [size];
 
                 for (int j=0; j<part; j++) {
                     for (int i=0; i<part; i++) {
@@ -118,19 +117,19 @@ void SolverCG::SetMPI(int Nx, int Ny, int p)
                         process_end_x[j*part+i] = part_end_x[i];
                         process_start_y[j*part+i] = part_start_y[j];
                         process_end_y[j*part+i] = part_end_y[j];
-						length[j*part+i] = (1 + part_end_x[i] - part_start_x[i])*(1 + part_end_y[j] - part_start_y[j]);
-						count_x[j*part+i] = i;
-						count_y[j*part+i] = j;
+			length[j*part+i] = (1 + part_end_x[i] - part_start_x[i])*(1 + part_end_y[j] - part_start_y[j]);
+			count_x[j*part+i] = i;
+			count_y[j*part+i] = j;
                     }
                 }
-				// Store first sub-domain positions at rank 0
-				x_start = process_start_x[0];
-				x_end = process_end_x[0];
-				y_start = process_start_y[0];
-				y_end = process_end_y[0];
-				ArraySize = length[0];
-				pos_x = count_x[0];
-				pos_y = count_y[0];
+		// Store first sub-domain positions at rank 0
+		x_start = process_start_x[0];
+		x_end = process_end_x[0];
+		y_start = process_start_y[0];
+		y_end = process_end_y[0];
+		ArraySize = length[0];
+		pos_x = count_x[0];
+		pos_y = count_y[0];
                 
                 // Sending sub-domain positions and information of loaction to the different processes
                 for (int dest=1; dest<size; dest++) {
@@ -138,9 +137,9 @@ void SolverCG::SetMPI(int Nx, int Ny, int p)
                     MPI_Send(&process_end_x[dest], 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
                     MPI_Send(&process_start_y[dest], 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
                     MPI_Send(&process_end_y[dest], 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
-					MPI_Send(&length[dest], 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
-					MPI_Send(&count_x[dest], 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
-					MPI_Send(&count_y[dest], 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
+		    MPI_Send(&length[dest], 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
+		    MPI_Send(&count_x[dest], 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
+		    MPI_Send(&count_y[dest], 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
                 }
                 
                 // Deallocating the temporary arrays
@@ -152,34 +151,34 @@ void SolverCG::SetMPI(int Nx, int Ny, int p)
                 delete[] part_start_y;
                 delete[] part_end_x;
                 delete[] part_end_y;
-				delete[] length;
-				delete[] count_x;
-				delete[] count_y;
-			}
-			else {
-				// Recieving sub-domain positions and location information on each process 
+		delete[] length;
+		delete[] count_x;
+		delete[] count_y;
+	    }
+	    else {
+	        // Recieving sub-domain positions and location information on each process 
                 MPI_Recv(&x_start, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-				MPI_Recv(&x_end, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-				MPI_Recv(&y_start, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-				MPI_Recv(&y_end, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-				MPI_Recv(&ArraySize, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-				MPI_Recv(&pos_x, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-				MPI_Recv(&pos_y, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            }
+		MPI_Recv(&x_end, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Recv(&y_start, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Recv(&y_end, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Recv(&ArraySize, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Recv(&pos_x, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Recv(&pos_y, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	    }
 			
-			// Compute additional sub-domain information for SolverCG calculations
-			height = (1+y_end-y_start);
-			width = (1+x_end-x_start);
-			j_start = y_start;
-			i_start = x_start;
-			j_end = y_end;
-			i_end = x_end;
-			n_layered = (height+2)*(width+2);
-			// Provide new start and end positions if sub-domain is within domain boundary (to facilitate some calculations)
-			if (y_start == 0){ j_start = y_start+1;}
-			if (x_start == 0){ i_start = x_start+1;}
-			if (y_end == Ny-1){ j_end = y_end-1;}
-			if (x_end == Nx-1){ i_end = x_end-1;}
+	    // Compute additional sub-domain information for SolverCG calculations
+	    height = (1+y_end-y_start);
+	    width = (1+x_end-x_start);
+	    j_start = y_start;
+	    i_start = x_start;
+	    j_end = y_end;
+	    i_end = x_end;
+	    n_layered = (height+2)*(width+2);
+	    // Provide new start and end positions if sub-domain is within domain boundary (to facilitate some calculations)
+	    if (y_start == 0){ j_start = y_start+1;}
+	    if (x_start == 0){ i_start = x_start+1;}
+	    if (y_end == Ny-1){ j_end = y_end-1;}
+	    if (x_end == Nx-1){ i_end = x_end-1;}
         }
     }
 }
@@ -187,7 +186,7 @@ void SolverCG::SetMPI(int Nx, int Ny, int p)
 /**
     @brief Function that solves the Poisson problem and computes streamfunction at time t + dt
     @param v Vorticity array (full domain)
-	@param s Streamfunction array (full domain)
+    @param s Streamfunction array (full domain)
     @return void
 */
 void SolverCG::Solve(double* v, double* s) {
@@ -227,7 +226,7 @@ void SolverCG::Solve(double* v, double* s) {
 		}
 	}
 
-    it = 0;
+        it = 0;
 	if (size > 1) {
 		// Obtain vorticity and sreamfunction sub-domain array of respective rank
 		CreatePartitions(v, s, b, x);
@@ -293,7 +292,7 @@ void SolverCG::Solve(double* v, double* s) {
 		b = v;
 		x = s;
 		
-	    ApplyOperator(x, t);
+	        ApplyOperator(x, t);
 		Copy(b,r);
 		ImposeBC(r);
 		Daxpy(t,r,-1.0);
@@ -329,7 +328,7 @@ void SolverCG::Solve(double* v, double* s) {
 		b = v;
 		x = s;
 		
-	    ApplyOperator(x, t);
+	        ApplyOperator(x, t);
 		cblas_dcopy(n, b, 1, r, 1);        // r_0 = b 
 		ImposeBC(r);
 		cblas_daxpy(n, -1.0, t, 1, r, 1);
@@ -405,11 +404,11 @@ void SolverCG::UpdateStreamfunction(double* stream){
 /**
     @brief Function to create sub-domain arrays from full domain arrays
     @param in1,in2 Entering full domain arrays
-	@param out1,out2 Exiting sub-domain arrays
+    @param out1,out2 Exiting sub-domain arrays
     @return void
 */
 void SolverCG::CreatePartitions(double* in1, double* in2, double* out1, double* out2) {
-    int i, j;
+        int i, j;
 	int k = 0;
 	for (i = x_start; i < 1+x_end; ++i) {
 		for (j = y_start; j < 1+y_end; ++j) {
@@ -423,12 +422,12 @@ void SolverCG::CreatePartitions(double* in1, double* in2, double* out1, double* 
 /**
     @brief Function to apply equation 4 to each interior value of domain
     @param in Entering sub-domain array (with additional outer layer)
-	@param out Exiting array (with size equal to unlayered sub-domain array)
+    @param out Exiting array (with size equal to unlayered sub-domain array)
     @return void
 */
 void SolverCG::ApplyOperator(double* in, double* out) {
-    double dx2i = 1.0/dx/dx;
-    double dy2i = 1.0/dy/dy;
+        double dx2i = 1.0/dx/dx;
+        double dy2i = 1.0/dy/dy;
 	if (size > 1) {
 		for (int i = i_start; i < 1+i_end; ++i) {
 			for (int j = j_start; j < 1+j_end; ++j) {
@@ -458,14 +457,14 @@ void SolverCG::ApplyOperator(double* in, double* out) {
 /**
     @brief Function to apply a scaling factor to interior values of domain
     @param in Entering array
-	@param out Exiting array with scaling factor
+    @param out Exiting array with scaling factor
     @return void
 */
 void SolverCG::Precondition(double* in, double* out) {
 	int k = 0;
-    double dx2i = 1.0/dx/dx;
-    double dy2i = 1.0/dy/dy;
-    double factor = 2.0*(dx2i + dy2i);
+        double dx2i = 1.0/dx/dx;
+        double dy2i = 1.0/dy/dy;
+        double factor = 2.0*(dx2i + dy2i);
 	if (size > 1) {
 		for (int i = x_start; i < 1+x_end; ++i) {
 			for (int j = y_start; j < 1+y_end; ++j) {
@@ -532,7 +531,7 @@ void SolverCG::ImposeBC(double* inout) {
 /**
     @brief Function that converts sub-domain array to full domain array
     @param x Sub-domain array
-	@param x_full Full domain array
+    @param x_full Full domain array
     @return void
 */
 void SolverCG::PartitionToDomain(double* x, double* x_full) {
@@ -548,7 +547,7 @@ void SolverCG::PartitionToDomain(double* x, double* x_full) {
 /**
     @brief Function to augment sub-domain array with an outer layer required for ApplyOperator calculations
     @param p Sub-domain array
-	@param p_layered Layered sub-domain array with values from neigbouring sub-domains
+    @param p_layered Layered sub-domain array with values from neigbouring sub-domains
     @return void
 */
 void SolverCG::AddLayer(double* p, double* p_layered) {
@@ -557,21 +556,21 @@ void SolverCG::AddLayer(double* p, double* p_layered) {
     double left_send[height];
     double right_send[height];
     double top_send[width];
-	double bottom_send[width];
+    double bottom_send[width];
     double left_recv[height];
     double right_recv[height];
     double top_recv[width];
     double bottom_recv[width];
 
     /// Sending layers
-	/// tags: left(0) right(1) top(2) bottom(3)
+    /// tags: left(0) right(1) top(2) bottom(3)
 	
 	// Left
     if (pos_x != 0) {
         for (int j = 0; j < height; ++j) {
             left_send[j] = p[j];
         }
-		// Send left layer to rank to the left (rank-1) 
+	// Send left layer to rank to the left (rank-1) 
         MPI_Send(left_send, height, MPI_DOUBLE, rank-1, 0, MPI_COMM_WORLD);
     }
     // Right
@@ -579,7 +578,7 @@ void SolverCG::AddLayer(double* p, double* p_layered) {
         for (int j = 0; j < height; ++j) {
             right_send[j] = p[(width-1)*height + j];
         }
-		// Send right layer to rank to the right (rank+1)
+	// Send right layer to rank to the right (rank+1)
         MPI_Send(right_send, height, MPI_DOUBLE, rank+1, 1, MPI_COMM_WORLD);
     }
 	// Top 
@@ -587,7 +586,7 @@ void SolverCG::AddLayer(double* p, double* p_layered) {
         for (int i = 0; i < width; ++i) {
             top_send[i] = p[i*height + (height-1)];
         }
-		// Send top layer to rank to the top (rank+p)
+	// Send top layer to rank to the top (rank+p)
         MPI_Send(top_send, width, MPI_DOUBLE, rank+part, 2, MPI_COMM_WORLD);
     }
 	// Bottom 
@@ -595,7 +594,7 @@ void SolverCG::AddLayer(double* p, double* p_layered) {
         for (int i = 0; i < width; ++i) {
             bottom_send[i] = p[i*height];
         }
-		// Send botttom layer to rank to the bottom (rank-p)
+	// Send botttom layer to rank to the bottom (rank-p)
         MPI_Send(bottom_send, width, MPI_DOUBLE, rank-part, 3, MPI_COMM_WORLD);
     }
 	
@@ -604,7 +603,7 @@ void SolverCG::AddLayer(double* p, double* p_layered) {
 	
 	// Left
     if (pos_x != 0) {
-		// Recieve new left layer (right layer wtr to other rank) from rank to the left (rank-1)
+	// Recieve new left layer (right layer wtr to other rank) from rank to the left (rank-1)
         MPI_Recv(left_recv, height, MPI_DOUBLE, rank-1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         for (int j = 0; j < height; ++j) {
             p_layered[j+1] = left_recv[j];
@@ -612,7 +611,7 @@ void SolverCG::AddLayer(double* p, double* p_layered) {
     }
     // Right
     if (pos_x != part-1) {
-		// Recieve new rght layer (left layer wtr to other rank) from rank to the right (rank+1)
+	// Recieve new rght layer (left layer wtr to other rank) from rank to the right (rank+1)
         MPI_Recv(right_recv, height, MPI_DOUBLE, rank+1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         for (int j = 0; j < height; ++j) {
             p_layered[(width+1)*(height+2)+(j+1)] = right_recv[j];
@@ -620,7 +619,7 @@ void SolverCG::AddLayer(double* p, double* p_layered) {
     }
     // Top
     if (pos_y != part-1) {
-		// Recieve new top layer (bottom layer wtr to other rank) from rank to the top (rank+p)
+	// Recieve new top layer (bottom layer wtr to other rank) from rank to the top (rank+p)
         MPI_Recv(top_recv, width, MPI_DOUBLE, rank+part, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         for (int i = 0; i < width; ++i) {
             p_layered[(i+1)*(height+2)+(height+1)] = top_recv[i];
@@ -628,19 +627,19 @@ void SolverCG::AddLayer(double* p, double* p_layered) {
     }
     // Bottom
     if (pos_y != 0) {
-		// Recieve new bottom layer (top layer wtr to other rank) from rank to the bottom (rank-p)
+	// Recieve new bottom layer (top layer wtr to other rank) from rank to the bottom (rank-p)
         MPI_Recv(bottom_recv, width, MPI_DOUBLE, rank-part, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         for (int i = 0; i < width; ++i) {
             p_layered[(i+1)*(height+2)] = bottom_recv[i];
         }
     }
 	
-	// Once sub-domain layer created from neighbouring data, add initial sub-domain data to new array
-	for (int i = 0; i < width; ++i) {
-		for (int j = 0; j < height; ++j) {
-			p_layered[(i+1)*(height+2)+(j+1)] = p[i*height + j];
-		}
+    // Once sub-domain layer created from neighbouring data, add initial sub-domain data to new array
+    for (int i = 0; i < width; ++i) {
+	for (int j = 0; j < height; ++j) {
+	    p_layered[(i+1)*(height+2)+(j+1)] = p[i*height + j];
 	}
+    }
 }
 
 /**
@@ -663,7 +662,7 @@ double SolverCG::DotProd(double* in1, double* in2) {
 /**
     @brief Function to copy array to another (used to parallelise cblas_dcopy with OpenMP)
     @param in Entering array to be copied
-	@param out New copied array
+    @param out New copied array
     @return void
 */
 void SolverCG::Copy(double* in, double* out) {
@@ -679,8 +678,8 @@ void SolverCG::Copy(double* in, double* out) {
 /**
     @brief Function that performs cblas_daxpy with for loops (used to parallelise cblas_daxpy with OpenMP)
     @param in Equivalent to x in daxpy
-	@param out Equivalent to y in daxpy
-	@param c Equivalent to alpha in daxpy 
+    @param out Equivalent to y in daxpy
+    @param c Equivalent to alpha in daxpy 
     @return void
 */
 void SolverCG::Daxpy(double* in, double* out, double c) {
